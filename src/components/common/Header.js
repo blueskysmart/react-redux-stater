43d05cfd12c7898883b64b583e58daa0f9623e37
AzureAD\CuongPhuNguyen <cuongphu.nguyen@hubcba.com>
2018-01-09 17:56:26 +0700

    bootstrap enhancement

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,12 +3,12 @@ import { Link, IndexLink } from 'react-router';
 
 const Header = () => {
     return (
-        <nav>
-            <IndexLink to="/" activeClassName="active">Home</IndexLink>
-            {" | "}
-            <Link to="/about" activeClassName="active">About</Link>
-            {" | "}
-            <Link to="/courses" activeClassName="active">Courses</Link>
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+                <li className="breadcrumb-item"><Link to="/about" activeClassName="active">About</Link></li>
+                <li className="breadcrumb-item active" aria-current="page"><Link to="/courses" activeClassName="active">Courses</Link></li>
+            </ol>
         </nav>
     );
 };
